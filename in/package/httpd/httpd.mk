c19efd6e454bacaa184ffe14713d5f1a71850359
corvus_ch <christian.haeusler@md-systems.ch>
2014-10-29 19:22:25 +0100

    Clean up httpds directory and files layout

--- in/package/httpd/httpd.mk
+++ in/package/httpd/httpd.mk
@@ -15,16 +15,17 @@ HTTPD_INSTALL_STAGING = YES
 # so we need to autoreconf:
 HTTPD_AUTORECONF      = YES
 
-HTTPD_CONF_ENV += \
+HTTPD_CONF_ENV = \
 	CC_FOR_BUILD="$(HOSTCC)" \
 	CFLAGS_FOR_BUILD="$(HOST_CFLAGS)" \
 	ap_cv_void_ptr_lt_long=no \
 	PCRE_CONFIG=$(STAGING_DIR)/usr/bin/pcre-config
 
-HTTPD_CONF_OPT += \
+HTTPD_CONF_OPT = \
+	--prefix= \
+	--enable-layout=Debian \
 	--sysconfdir=/etc/apache2 \
-	--enable-module=all \
-	--enable-so \
+	--disable-userdir \
 	--with-apr=$(STAGING_DIR)/usr/bin/apr-1-config \
 	--with-apr-util=$(STAGING_DIR)/usr/bin/apu-1-config \
 	--with-pcre=$(STAGING_DIR)/usr/bin/pcre-config
@@ -32,11 +33,5 @@ HTTPD_CONF_OPT += \
 ifeq ($(BR2_ARCH_HAS_ATOMICS),y)
 HTTPD_CONF_OPTS += --enable-nonportable-atomics=yes
 endif
-
-define HTTPD_FIX_STAGING_HTTPD_CONFIG
-	$(SED) 's%/usr/build%$(STAGING_DIR)/usr/build%' $(STAGING_DIR)/usr/bin/apxs
-	$(SED) 's%^prefix =.*%prefix = $(STAGING_DIR)/usr%' $(STAGING_DIR)/usr/build/config_vars.mk
-endef
-HTTPD_POST_INSTALL_STAGING_HOOKS += HTTPD_FIX_STAGING_HTTPD_CONFIG
 
 $(eval $(autotools-package))
